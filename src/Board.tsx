d56799790c146564b74d8a609b5c8a6429bcc83c
leonelhrp <leonelrojasperez@gmail.com>
2023-09-12 13:22:58 -0300

    feat: add animation when is match

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card.tsx';
 
 interface BoardProps {
@@ -12,8 +12,27 @@ const Board: React.FC<BoardProps> = ({
   flippedCards,
   onCardClick,
 }) => {
+  const [showCelebration, setShowCelebration] = useState(false);
+
+  useEffect(() => {
+    if (flippedCards.length === 2) {
+      const [firstIndex, secondIndex] = flippedCards;
+      if (animals[firstIndex].name === animals[secondIndex].name) {
+        setShowCelebration(true);
+        setTimeout(() => setShowCelebration(false), 1000);
+      }
+    }
+  }, [flippedCards]);
+
   return (
-    <div className="board flex flex-wrap justify-center mt-10">
+    <div className="board flex flex-wrap justify-center">
+      {showCelebration && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="animate-pulse text-3xl font-bold text-green-500">
+            Match!
+          </div>
+        </div>
+      )}
       {animals.map((animal, index) => (
         <Card
           key={index}
